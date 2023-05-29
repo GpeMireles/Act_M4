dc58165d36b7fdc172192f6e863e51aa8f966559
GpeMireles <guadalupe__mireles@hotmail.com>
2023-05-28 23:48:02 -0600

    Program.cs doxygen format

--- grpcClient/Program.cs
+++ grpcClient/Program.cs
@@ -1,26 +1,19 @@
-﻿using System;
-using System.Threading.Tasks;
-using Grpc.Net.Client;
-using Grpc.Core;
-
-namespace grpcClient {
+﻿namespace grpcClient {
+    /**
+     * @class Program
+     * @brief Clase principal que contiene el punto de entrada del cliente gRPC.
+     */
     class Program {
+        /**
+         * @brief Punto de entrada del programa.
+         */
         static void Main() {
 
-            Console.WriteLine("Searching channel...");
             var channel = new Grpc.Core.Channel("127.0.0.1:50052", ChannelCredentials.Insecure);
-            //using var channel = GrpcChannel.ForAddress("https://localhost:50052");
             var client = new GrpcCoords.Coords.CoordsClient(channel);
             var reply = client.GetCoords(new Google.Protobuf.WellKnownTypes.Empty());
-            Console.WriteLine("Competleted ...");
             Console.WriteLine(reply.Data);
-           // await channel.Shutdown();
-            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
-            Console.WriteLine("Hello World!");
-
         }
     }
 }
-// The port number must match the port of the gRPC server.
-
