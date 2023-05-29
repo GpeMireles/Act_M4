2df6ac18e1c2eb5659ed274de98a14bf53fc6a0e
GpeMireles <guadalupe__mireles@hotmail.com>
2023-05-28 23:35:07 -0600

    coords_server doxygen format

--- proto/coords_server.py
+++ proto/coords_server.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
-# Detect green object through webcam and print coordinates
 
-# Ros & libraries
+"""
+@file corrds_server.py
+@brief Lee el tópico con las coordenadas y genera un servicio grpc.
+"""
+
 import cv2
 import numpy as np
 import ctypes
@@ -11,7 +14,6 @@ from ctypes import *
 import signal
 import threading
 
-# gRPC libraries
 import grpc
 from concurrent import futures
 import time
@@ -24,7 +26,7 @@ class CoordsService:
 
     def __init__(self):
         rospy.Subscriber("/coord", Float64MultiArray, self.getCoordsCallback)
-        self.coords = [0,0,0]
+        self.coords = [0, 0, 0]
     
     def getCoordsCallback(self, msg):
         self.coords[0] = msg.data[0]
@@ -32,14 +34,25 @@ class CoordsService:
         self.coords[2] = msg.data[2]
 
     def GetCoords(self, request, context):
+        """
+        Método para obtener las coordenadas.
 
+        :param request: Solicitud vacía.
+        :param context: Contexto de la llamada.
+        :return: Respuesta con las coordenadas y timestamp.
+        """
         response = pb2.MessageResponse()
-        response.data.extend(self.coords)  # Ejemplo de respuesta con valores de prueba
+        response.data.extend(self.coords)
         print("Response")
         return response
-
  
 def terminate_server(signum, frame):
+    """
+    Función para terminar el servidor.
+
+    :param signum: Número de señal.
+    :param frame: Marco de la señal.
+    """
     print("Got signal: ", frame)
     rospy.signal_shutdown("Ending rosnode")
     terminate.set()
@@ -58,41 +71,3 @@ if __name__ == '__main__':
     rospy.spin()
     print("Exit")
     terminate.wait()
-
-
-
-
-
-
-
-
-# import grpc
-# from concurrent import futures
-# import time
-# import coords_pb2_grpc as pb2_grpc
-# import coords_pb2 as pb2
-
-
-# class CoordsService(pb2_grpc.CoordsServicer):
-
-#     def __init__(self, *args, **kwargs):
-#         pass
-
-#     def GetCoords(self, request, context):
-        
-#         response = pb2.MessageResponse()
-#         response.data.extend([1.0, 2.0, 3.0])  # Ejemplo de respuesta con valores de prueba
-#         print("Response")
-#         return response
-
-
-# def serve():
-#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-#     pb2_grpc.add_CoordsServicer_to_server(CoordsService(), server)
-#     server.add_insecure_port('[::]:50052')
-#     server.start()
-#     server.wait_for_termination()
-
-
-# if __name__ == '__main__':
-#     serve()
